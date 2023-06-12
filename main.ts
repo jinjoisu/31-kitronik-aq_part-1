78f4ea30a14fd4adfdd47dbbf53ff008d95089f9
jinjoisu <133915914+jinjoisu@users.noreply.github.com>
2023-06-12 09:56:28 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     kitronik_air_quality.measureData()
-    kitronik_air_quality.show(kitronik_air_quality.getAirQualityScore(), 1)
+    kitronik_air_quality.show(kitronik_air_quality.getAirQualityScore(), 1, kitronik_air_quality.ShowAlign.Left)
 })
 input.onButtonPressed(Button.AB, function () {
     kitronik_air_quality.measureData()
-    kitronik_air_quality.show(kitronik_air_quality.getAirQualityPercent(), 3)
+    kitronik_air_quality.show(kitronik_air_quality.getAirQualityPercent(), 3, kitronik_air_quality.ShowAlign.Left)
 })
 input.onButtonPressed(Button.B, function () {
     kitronik_air_quality.measureData()
-    kitronik_air_quality.show(kitronik_air_quality.readeCO2(), 2)
+    kitronik_air_quality.show(kitronik_air_quality.readeCO2(), 2, kitronik_air_quality.ShowAlign.Left)
 })
 kitronik_air_quality.setupGasSensor()
 kitronik_air_quality.calcBaselines()
